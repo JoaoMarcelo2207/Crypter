ff2e3c8265a9636919e1bdfe546147ff06ed55c2
João Marcelo Costa Miranda <jmcostamiranda@hotmail.com>
2023-07-10 19:55:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("CodifyButton").addEventListener("click", function() {
+    var texto = document.getElementById("CodifyTextBox").value;
+    var key = document.getElementById("KeyTextBox").value;
+  });
+  
