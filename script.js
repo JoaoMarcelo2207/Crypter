c3854e9432c9dc4ef7bfb4dcd4c02f0e63f00035
João Marcelo Costa Miranda <jmcostamiranda@hotmail.com>
2023-07-10 22:00:43 -0300

    adjust

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 document.getElementById("CodifyButton").addEventListener("click", function() {
-    var texto = document.getElementById("CodifyTextBox").value;
+    var message = document.getElementById("CodifyTextBox").value;
     var key = document.getElementById("KeyTextBox").value;
+    if(onlyNumbers(key)) return;
+    message = codify(message, key);
+    alert(message);
   });
+
+function onlyNumbers(x){
+  if(isNaN(x))alert("Please type a number!");
+}
+
+function codify(text, key) {
+
+}
+
+function decodify(text, key){
   
+}
+
